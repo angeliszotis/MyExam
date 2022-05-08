06f150c5ada5171980e56287fb9eeb0817a91475
Stelios <stylianosplus@gmail.com>
2022-05-09 00:10:46 +0300

    added real data

--- MyExamProject/MyExamReact/src/components/Exam.js
+++ MyExamProject/MyExamReact/src/components/Exam.js
@@ -13,12 +13,51 @@ import TableHead from '@mui/material/TableHead';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 
+const columns = [
+    { id: 'name', label: 'Name', minWidth: 150 },
+    { id: 'description', label: 'Description', minWidth: 100 },
+    {
+        id: 'owner',
+        label: 'Owner',
+        minWidth: 20,
+        align: 'right',
+        format: (value) => value.toLocaleString('en-US'),
+    },
+    {
+        id: 'date',
+        label: 'Date',
+        minWidth: 200,
+        align: 'right',
+        format: (value) => value.toLocaleString('en-US'),
+    },
+    {
+        id: 'start',
+        label: 'Start exam',
+        minWidth: 170,
+        align: 'right',
+        format: (value) => value.toLocaleString('en-US'),
+    },
+];
+
+
 export default function Exam() {
 
 
     const { context, setContext } = useStateContext()
     const [qns, setQns] = useState([])
     const [qnIndex, setQnIndex] = useState(0)
+
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(+event.target.value);
+        setPage(0);
+    };
 
 
     useEffect(() => {
@@ -34,25 +73,47 @@ export default function Exam() {
 
     }, [])
     return (
-        qns.length != 0
-            ? <Card>
-                <CardContent>
-                    {qns[qnIndex].id}
-                    <List>
-                        {qns.map((data, idx) => (
-                            <ListItemButton >
-                                <div>{data.name}</div>
-                                <div>{data.description}</div>
-                                <div>{data.owner}</div>
-                                <div>{data.date}</div>
-                            </ListItemButton >
+        <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+            <TableContainer sx={{ maxHeight: 440 }}>
+                <Table stickyHeader aria-label="sticky table">
+                    <TableHead>
+                        <TableRow>
+                            {columns.map((column) => (
+                                <TableCell
+                                    key={column.id}
+                                    align={column.align}
+                                    style={{ minWidth: column.minWidth }}
+                                >
+                                    {column.label}
+                                </TableCell>
+                            ))}
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {qns
+                            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                            .map((data, idx) => (
+                                <TableRow >
+                                    <TableCell >{data.name}</TableCell >
+                                    <TableCell >{data.description}</TableCell >
+                                    <TableCell >{data.owner}</TableCell >
+                                    <TableCell >{data.date}</TableCell >
+                                </TableRow >
 
-                        ))}
-                    </List>
-                </CardContent>
-            </Card>
-            : null
-
-    )
+                            ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <TablePagination
+                rowsPerPageOptions={[10, 25, 100]}
+                component="div"
+                count={qns.length}
+                rowsPerPage={qns}
+                page={page}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+        </Paper>
+    );
 
 }
